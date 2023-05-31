4e0951118d6d63aed52c6e25e4e95184189981dc
Tidiane <camaret.tidiane@gmail.com>
2023-05-31 12:31:52 +0000

    ss

--- scripts/hpo/sweep.py
+++ scripts/hpo/sweep.py
@@ -1,3 +1,7 @@
+"""
+Run sweep using CUDA_VISIBLE_DEVICES=0 wandb agent tidiane/EEGClip_HPO/2omz42gc
+"""
+
 # Import the W&B Python Library and log into W&B
 import wandb
 from scripts.clip.clip_tuh_eeg import run_training
@@ -26,19 +30,23 @@ def main():
         batch_size = wandb.config.batch_size
     )
 
+    torch.cuda.empty_cache()
+
+
 # 2: Define the search space
 sweep_configuration = {
+    #'progam': 'sweep.py',
     'method': 'bayes',
     'metric': 
     {
         'goal': 'maximize', 
         'name': 'logreg'
         },
-    'early_terminate':
-        {'type': 'hyperband',
-        'min_iter': 2,
-        'eta':2
-        },
+    #'early_terminate':
+    #    {'type': 'hyperband',
+    #    'min_iter': 2,
+    #    'eta':2
+    #    },
     'parameters': 
     {
         'lr': {'distribution':'log_uniform_values','min': 1e-5, 'max': 1e-1,},
@@ -57,4 +65,5 @@ sweep_id = wandb.sweep(
     project='EEGClip_HPO'
     )
 
-wandb.agent(sweep_id, function=main, count=10)
+#if __name__ == "__main__":
+#    main()
