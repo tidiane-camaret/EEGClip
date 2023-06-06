17ac923587bb34662da4a2db2a129bff11e5a342
Tidiane <camaret.tidiane@gmail.com>
2023-06-06 19:25:59 +0000

    refactor a bit

--- scripts/old/classif_tuh_abnormal.py
+++ scripts/old/classif_tuh_abnormal.py
@@ -1,142 +1,269 @@
-
-import numpy as np
-import matplotlib.pyplot as plt
-import mne
+import argparse
+import socket
 import pandas as pd
+import numpy as np
+import torch 
+
+from braindecode.datasets.base import BaseConcatDataset
 from braindecode.datasets import TUHAbnormal
-from braindecode.preprocessing import (
-    preprocess, Preprocessor, create_fixed_length_windows, create_windows_from_events, scale as multiply)
-import torch
+from braindecode.preprocessing import create_fixed_length_windows
 from braindecode.util import set_random_seeds
-from braindecode.models import ShallowFBCSPNet, deep4
-from skorch.callbacks import LRScheduler
-from skorch.helper import predefined_split
-from pytorch_lightning.callbacks.progress import TQDMProgressBar
-from pytorch_lightning.loggers import TensorBoardLogger
+from braindecode.models.util import to_dense_prediction_model, get_output_shape
+from braindecode.datasets.tuh import TUHAbnormal
+from braindecode.preprocessing import preprocess, Preprocessor
+from braindecode.datasets.base import BaseConcatDataset
+from braindecode.preprocessing.windowers import create_fixed_length_windows
+from braindecode.models import Deep4Net
+
 from pytorch_lightning import Trainer
-from braindecode import EEGClassifier
+from pytorch_lightning.loggers import WandbLogger
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
 
-from EEGClip.classifier_models import EEGClassifierModule
+from EEGClip.classifier_models import EEGClassifierModel
+from EEGClip.clip_models import EEGClipModel
 
+import mne
 mne.set_log_level('ERROR')  # avoid messages everytime a window is extracted
 
-TUHAbnormal_PATH = '/home/jovyan/mne_data/TUH/tuh_eeg_abnormal/v2.0.0'
-N_JOBS = 8  # specify the number of jobs for loading and windowing
-N_SAMPLES = 50
-
-tuh = TUHAbnormal(
-    path=TUHAbnormal_PATH,
-    #recording_ids=list(range(N_SAMPLES)),
-    target_name=('pathological'),#'report'),
-    preload=False,
-    add_physician_reports=True,
-    n_jobs=N_JOBS, 
-)
-
-print("length of dataset : ", len(tuh))
-
-
-print(tuh.description)
-
-# create windows
-
-window_size_samples = 1000
-window_stride_samples = 1000
-tuh_windows = create_fixed_length_windows(
-    tuh,
-    window_size_samples=window_size_samples,
-    window_stride_samples=window_stride_samples,
-    drop_last_window=False,
-    n_jobs=N_JOBS,
-
-)
+import os
 
 """
-tuh_windows.save(
-    path="data/TUH_abnormal_sample",
-    overwrite=True,
-)
+This script is used to train a classifier model
+on the TUH EEG dataset for different classification tasks :
+pathological
+age
+gender
+report-based (medication, diagnosis ...)
+
 """
-print(len(tuh_windows))
+#MODEL_PATH = '/home/jovyan/EEGClip/results/wandb/EEGClip/df7e5wqd/checkpoints/epoch=7-step=48696.ckpt'
+MODEL_PATH = "/home/jovyan/EEGClip/results/wandb/EEGClip/1lgwz214/checkpoints/epoch=6-step=42609.ckpt"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Train an EEG classifier on the TUH EEG dataset.')
+    parser.add_argument('--task_name', type=str, default="pathological",
+                        help='classification task name (pathological, age, gender, report-related tasks ....')    
+    parser.add_argument('--n_rec', type=int, default=2993,
+                        help='Number of recordings to load from TUH EEG dataset.')
+    parser.add_argument('--n_epochs', type=int, default=10,
+                        help='Number of epochs to train EEGClip model.')
+    parser.add_argument('--batch_size', type=int, default=64,
+                        help='Batch size to train EEGClip model.')
+    parser.add_argument('--lr', type=float, default=5e-3,
+                        help='Learning rate to train EEGClip model.')
+    parser.add_argument('--weight_decay', type=float, default=5e-4,
+                        help='Weight decay to train EEGClip model.')
+    #parser.add_argument('--nailcluster', action='store_true',
+    #                    help='Whether to run on the Nail cluster(paths differ)')
+    parser.add_argument('--num_workers', type=int, default=16,
+                        help='Number of workers to use for data loading.')
+    parser.add_argument('--weights', type=str, default="eegclip",
+                        help='weights from pretrained model, or random')    
+    parser.add_argument('--freeze_encoder', action='store_true',
+                        help='Whether to freeze encoder during training')
 
-# split the dataset in train and test (label is included in the data)
-splitted = tuh_windows.split("train")
-train_set = splitted['True']
-valid_set = splitted['False']
+    args = parser.parse_args()
 
+    num_workers = args.num_workers
 
-cuda = torch.cuda.is_available()  # check if GPU is available, if True chooses to use it
-device = 'cuda' if cuda else 'cpu'
-if cuda:
+    n_recordings_to_load = args.n_rec
+    task_name = args.task_name #('pathological', 'age', 'gender', "epilep", "keppra", "dilantin", "seizure")
+    if task_name in ['pathological', 'age', 'gender']: 
+        target_name = task_name 
+    elif task_name == "age_50":
+        target_name = "age"
+    else:
+        target_name = "report"
+    mapping = {'M': 0, 'F': 1} if target_name =="gender" else None
+    # TODO : find a way to use several targets
+    n_max_minutes = 3
+    sfreq = 100
+    n_minutes = 2
+    input_window_samples = 1200
+    
+    n_epochs = args.n_epochs
+    batch_size = args.batch_size
+    lr = args.lr
+    weight_decay = args.weight_decay
+    nailcluster = (socket.gethostname() == "vs3-0")
+    weights = args.weights
+    freeze_encoder = args.freeze_encoder
+    num_workers = args.num_workers
+    
+
+    if nailcluster:
+        results_dir = "/home/jovyan/EEGClip/results/"
+        tuh_data_dir = "/home/jovyan/mne_data/TUH_PRE/tuh_eeg_abnormal_clip/v2.0.0/edf/"
+    else:
+        results_dir = "/home/ndirt/dev/neuro_ai/EEGClip/results/"
+        tuh_data_dir = "/data/datasets/TUH/EEG/tuh_eeg_abnormal/v2.0.0/edf/"
+
+  # TODO : use get_output_shape (requires to load the model first)
+    n_preds_per_input = 519 #get_output_shape(eeg_classifier_model, n_chans, input_window_samples)[2]
+
+  
+    seed = 20210325  # random seed to make results reproducible
+
+    cuda = torch.cuda.is_available()
+    set_random_seeds(seed=seed, cuda=cuda)
     torch.backends.cudnn.benchmark = True
-seed = 20200220
-set_random_seeds(seed=seed, cuda=cuda)
 
-n_classes = 2
-# Extract number of chans and time steps from dataset
-n_chans = train_set[0][0].shape[0]
-input_window_samples = train_set[0][0].shape[1]
+    # ## Load data
+    dataset = TUHAbnormal(
+        path=tuh_data_dir,
+        recording_ids=range(n_recordings_to_load),  # loads the n chronologically first recordings
+        target_name=target_name,  # age, gender, pathology
+        preload=False,
+        add_physician_reports=True,
+        n_jobs=1)
+    
+    # ## Preprocessing
 
-eeg_classifier_model = ShallowFBCSPNet(
-    n_chans,
-    n_classes,
-    input_window_samples=input_window_samples,
-    final_conv_length='auto',
-)
+    ar_ch_names = sorted([
+        'EEG A1-REF', 'EEG A2-REF',
+        'EEG FP1-REF', 'EEG FP2-REF', 'EEG F3-REF', 'EEG F4-REF', 'EEG C3-REF',
+        'EEG C4-REF', 'EEG P3-REF', 'EEG P4-REF', 'EEG O1-REF', 'EEG O2-REF',
+        'EEG F7-REF', 'EEG F8-REF', 'EEG T3-REF', 'EEG T4-REF', 'EEG T5-REF',
+        'EEG T6-REF', 'EEG FZ-REF', 'EEG CZ-REF', 'EEG PZ-REF'])
 
-# Send model to GPU
+    preprocessors = [
+        Preprocessor(fn='pick_channels', ch_names=ar_ch_names, ordered=True),
+        Preprocessor('crop', tmin=0, tmax=n_max_minutes*60, include_tmax=True),
+        Preprocessor(fn=lambda x: np.clip(x, -800,800), apply_on_array=True),
+        Preprocessor('set_eeg_reference', ref_channels='average'),
+        # convert from volt to microvolt, directly modifying the numpy array
+        Preprocessor(fn=lambda x: x * 1e6, apply_on_array=True),
+        Preprocessor(fn=lambda x: x / 30, apply_on_array=True), # this seemed best
+        Preprocessor(fn='resample', sfreq=sfreq),
+    ]
+    # Preprocess the data
+    if not nailcluster:
+        preprocess(dataset, preprocessors)
+
+   # ## Data Splitting
+    # TODO : split using train and test splits instead
+    # TODO : maybe load TUH now on top of TUH Abnormal ?
+
+    
+    #dataset = dataset.split('train')['True']
+
+    subject_datasets = dataset.split('subject')
+    n_subjects = len(subject_datasets)
+
+    print("Nb subjects loaded : ", n_subjects)
+    
+    n_split = int(np.round(n_subjects * 0.75))
+    keys = list(subject_datasets.keys())
+    train_sets = [d for i in range(n_split) for d in subject_datasets[keys[i]].datasets]
+    train_set = BaseConcatDataset(train_sets)
+    valid_sets = [d for i in range(n_split, n_subjects) for d in subject_datasets[keys[i]].datasets]
+    valid_set = BaseConcatDataset(valid_sets)
 
 
-# These values we found good for shallow network:
-lr = 0.0625 * 0.01
-weight_decay = 0
+    window_train_set = create_fixed_length_windows(
+        train_set,
+        start_offset_samples=60*sfreq,
+        stop_offset_samples=60*sfreq+n_minutes*60*sfreq,
+        preload=True,
+        window_size_samples=input_window_samples,
+        window_stride_samples=n_preds_per_input,
+        drop_last_window=True,
+        mapping=mapping
+    )
 
-# For deep4 they should be:
-# lr = 1 * 0.01
-# weight_decay = 0.5 * 0.001
+    window_valid_set = create_fixed_length_windows(
+        valid_set,
+        start_offset_samples=60*sfreq,
+        stop_offset_samples=60*sfreq+n_minutes*60*sfreq,
+        preload=True,
+        window_size_samples=input_window_samples,
+        window_stride_samples=n_preds_per_input,
+        drop_last_window=False,
+        mapping=mapping
+    )
 
-batch_size = 64
-n_epochs = 50
+    ### PREPROCESSING NECESSARY IF USING TUH_PRE
+    if nailcluster:
+        window_train_set.transform = lambda x: x*1e6
+        window_valid_set.transform = lambda x: x*1e6
 
+    train_loader = torch.utils.data.DataLoader(
+        window_train_set,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=True)
+    train_det_loader = torch.utils.data.DataLoader(
+        window_train_set,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        drop_last=False)
+    valid_loader = torch.utils.data.DataLoader(
+        window_valid_set,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        drop_last=False)
 
-if cuda:
-    eeg_classifier_model.cuda()
+    print(len(valid_loader.dataset))
+    
 
-clf = EEGClassifier(
-    eeg_classifier_model,
-    criterion=torch.nn.NLLLoss,
-    optimizer=torch.optim.AdamW,
-    train_split=predefined_split(valid_set),  # using valid_set for validation
-    optimizer__lr=lr,
-    optimizer__weight_decay=weight_decay,
-    batch_size=batch_size,
-    callbacks=[
-        "accuracy", ("lr_scheduler", LRScheduler('CosineAnnealingLR', T_max=n_epochs - 1)),
-    ],
-    device=device,
-)
-# Model training for a specified number of epochs. `y` is None as it is already supplied
-# in the dataset.
-clf.fit(train_set, y=None, epochs=n_epochs)
-"""
+    encoder_output_dim = 16 # size of the last layer of the EEG decoder
+    n_chans = 21 # number of channels in the EEG data
 
-train_loader = torch.utils.data.DataLoader(train_set, 
-                                            batch_size = batch_size,
-                                            num_workers=32
+    # ## Create model
+    if weights == "eegclip":
+            eegclipmodel = EEGClipModel.load_from_checkpoint(MODEL_PATH)
+            EEGEncoder = torch.nn.Sequential(eegclipmodel.eeg_encoder,eegclipmodel.eeg_projection)
+            # get size of the last layer
+            projectionhead = list(EEGEncoder.children())[-1]
+            layer_sizes = []
+            for layer in projectionhead.children():
+                if hasattr(layer, 'out_features'):
+                    layer_sizes.append(layer.out_features)
+            encoder_output_dim = layer_sizes[-1]
+    elif weights == "random":
+        EEGEncoder = Deep4Net(
+            in_chans=n_chans,
+            n_classes=encoder_output_dim, 
+            input_window_samples=None,
+            final_conv_length=2,
+            stride_before_pool=True,
+            )
+
+        to_dense_prediction_model(EEGEncoder)
+
+        
+            # ## Run Training
+    wandb_logger = WandbLogger(project="EEGClip_classif",
+                        save_dir = results_dir + '/wandb',
+                        log_model=False,
+                        )
+
+    wandb_logger.experiment.config.update({"freeze_encoder": freeze_encoder,
+                                            "weights": weights,
+                                            "task_name": task_name,
+                                            "target_name": target_name},
+                                            #allow_val_change=True
                                             )
-valid_loader = torch.utils.data.DataLoader(valid_set, 
-                                            batch_size = batch_size,
-                                            num_workers=32)
-
-logger = TensorBoardLogger("results/tb_logs", name="EEG_Classifier")
-
-trainer = Trainer(
-    accelerator="auto",
-    devices=1 if torch.cuda.is_available() else None,  
-    max_epochs=n_epochs,
-    callbacks=[TQDMProgressBar(refresh_rate=20)],
-    logger=logger
-)
-
-trainer.fit(EEGClassifierModule(eeg_classifier_model=eeg_classifier_model, lr = lr), train_loader, valid_loader)
-"""
+    trainer = Trainer(
+                default_root_dir=results_dir + '/models',
+                devices=1,
+                accelerator="gpu",
+                max_epochs=n_epochs,
+                logger=wandb_logger,
+                #checkpoint_callback=False # do not save model
+            )
+    trainer.fit(
+        EEGClassifierModel(
+                  EEGEncoder, 
+                  task_name = task_name,
+                  freeze_encoder=freeze_encoder,
+                  lr = lr,
+                  weight_decay= weight_decay,
+                  encoder_output_dim = encoder_output_dim,
+        ),
+        train_loader,
+        valid_loader,
+    )
